3b51714f97bfe8268e57333c4beec28dfdc5d6ba
ssp13 <ssp13@users.noreply.github.com>
2014-08-12 17:42:29 +0300

    chnaged libraries added public channels

--- pusher_server.js
+++ pusher_server.js
@@ -12,12 +12,12 @@ app.all('*', function(req, res, next) {
   next();
  });
 app.use(express.static(__dirname + '/AC'));
-app.get('/api/get/on',function(req,res){
+app.get('/api/get/priv/on',function(req,res){
   
 pusher.trigger('private-channel', 'client-turnon', { message: "on" });
 res.send('sended channel-1');
 });
-app.get('/api/get/off',function(req,res){
+app.get('/api/get/priv/off',function(req,res){
   
 pusher.trigger('private-channel', 'client-turnoff', { message: "on" });
 res.send('sended channel-1');
@@ -27,8 +27,16 @@ app.get('/hello.txt', function(req, res){
 pusher.trigger('channel-1', 'test_event', { message: "hello world" });
   
 });
-    
-
+ app.get('/api/get/on',function(req,res){
+  
+pusher.trigger('ac_channel', 'turnoff', { message: "on" });
+res.send('sended ac_channel-turnoff');
+});   
+app.get('/api/get/off',function(req,res){
+  
+pusher.trigger('ac_channel', 'turnoff', { message: "off" });
+res.send('sended ac_channen-turnoff');
+});
 
 
 
