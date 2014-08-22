a0e6cb17a4fa2ebb3fa3a2138882ac07acc66c41
ssp13 <ssp13@users.noreply.github.com>
2014-08-22 21:13:06 +0300

    content type headers

--- pusher_server.js
+++ pusher_server.js
@@ -12,6 +12,7 @@ var pusher = new Pusher({
 var myFirebaseRef = new Firebase("https://crackling-fire-1603.firebaseio.com/");
 app.all('*', function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
   res.header("Access-Control-Allow-Headers", "X-Requested-With,Content-Type");
   
   next();
